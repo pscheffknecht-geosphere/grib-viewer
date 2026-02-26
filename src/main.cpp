4f527b18e225c750a1b073bb3069e29fe2019aa5
phillip <ph.scheffknecht@gmail.com>
2026-02-26 13:03:54 +0000

    make subdir for ui, move ui code there, make main window code file

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,16 @@
+#include <GLFW/glfw3.h>
 #include <imgui.h>
 #include <imgui_impl_glfw.h>
 #include <imgui_impl_opengl3.h>
-#include <GLFW/glfw3.h>
+
 #include <iostream>
 #include <string>
 
-#include "grib_reader.h"
-#include "renderer.h"
-#include "settings.h"
-#include "mpl_gradients.h"
-#include "messagesWindow.h"
+
+
+// ui stuff
+#include "ui/mainWindow.h"
+
 
 static void glfw_error_callback(int error, const char* description) {
     fprintf(stderr, "GLFW Error %d: %s\n", error, description);
@@ -83,31 +84,13 @@ int main(int argc, char** argv) {
     GribReader reader;
     Renderer renderer;
     char filename[512] = "";
-    int currentMessage = 0;
-    int previousMessage = -1;
 
-    // stuff for the main image
-    GLuint fieldTexture;
-    bool needNewTexture = true;
-    int displayWidth = 0;
-    int displayHeight = 0;
-    std::vector<Color> imgData;
-    ImVec2 yScanDirectionA = ImVec2(0, 0);
-    ImVec2 yScanDirectionB = ImVec2(1, 1);
-
-    // add a color bar
-    bool updateCbarTexture = true;
-    int cbarHeight = 20;
-    int cbarWidth = 500;
-    GLuint cbarTexture = renderer.createTexture(cbarWidth, cbarHeight);
-    std::vector<Color> cbarData;
-    cbarData.resize(cbarWidth * cbarHeight);
-
-    // sub windows
-    bool showMessageListWindow = true;
 
     // Load file from command line if provided
     std::vector<long> messageOffsets;
+    static ImVec2 yScanDirectionA = ImVec2(0, 0);
+    static ImVec2 yScanDirectionB = ImVec2(1, 1);
+
     if (argc > 1) {
         strcpy(filename, argv[1]);
         reader.loadFile(filename, yScanDirectionA, yScanDirectionB);
@@ -115,198 +98,7 @@ int main(int argc, char** argv) {
 
     // Main loop
     while (!glfwWindowShouldClose(window)) {
-        static GribViewerSettings settings;
-        static GribViewerSettings settings_old;
-
-        glfwPollEvents();
-        // ImGuiID dockspace_id = ImGui::GetID("GRIB Viewer");
-        // Start ImGui frame
-        ImGui_ImplOpenGL3_NewFrame();
-        ImGui_ImplGlfw_NewFrame();
-        ImGui::NewFrame();
-        ImGuiWindowFlags window_flags =
-            ImGuiWindowFlags_NoDocking |
-            ImGuiWindowFlags_NoTitleBar |
-            ImGuiWindowFlags_NoCollapse |
-            ImGuiWindowFlags_NoResize |
-            ImGuiWindowFlags_NoMove |
-            ImGuiWindowFlags_NoBringToFrontOnFocus |
-            ImGuiWindowFlags_NoNavFocus;
-
-        const ImGuiViewport* viewport = ImGui::GetMainViewport();
-        ImGui::SetNextWindowPos(viewport->WorkPos);
-        ImGui::SetNextWindowSize(viewport->WorkSize);
-        ImGui::SetNextWindowViewport(viewport->ID);
-
-        ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
-        ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
-
-        ImGui::Begin("DockSpaceHost", nullptr, window_flags);
-
-        ImGui::PopStyleVar(2);
-
-        ImGuiID dockspace_id = ImGui::GetID("MyDockSpace");
-        ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), ImGuiDockNodeFlags_None);
-
-        ImGui::End();
-        // Main window
-        // ImGui::SetNextWindowPos(ImVec2(0, 0));
-        // ImGui::SetNextWindowSize(ImGui::GetIO().DisplaySize);
-        ImGui::Begin("GRIB Viewer"); /*, nullptr, 
-                     ImGuiWindowFlags_NoTitleBar | 
-                     ImGuiWindowFlags_NoResize | 
-                     ImGuiWindowFlags_NoMove); */
-
-        // File selection
-        ImGui::Text("GRIB File:");
-        ImGui::SameLine();
-        ImGui::InputText("##filename", filename, sizeof(filename));
-        ImGui::SameLine();
-        
-        if (ImGui::Button("Load")) {
-            reader.close();
-            reader.loadFile(filename, yScanDirectionA, yScanDirectionB);
-        }
-
-        ImGui::Separator();
-        static bool updateImg = false;
-        if (reader.fileLoaded && reader.messageCount > 0) {
-            if (ImGui::Button("Show Messages Window")) {
-                showMessageListWindow = !showMessageListWindow;
-            }
-            bool showGribSortWindow = true;
-            gribSortWindow(&showGribSortWindow, reader.messageList);
-            if (showMessageListWindow) {
-                gribMessageListWindow(&showMessageListWindow, reader.messageList, currentMessage);
-            }
-            // Message selection
-            ImGui::Text("Message: %d / %d", currentMessage + 1, reader.messageCount);
-            ImGui::SliderInt("##message", &currentMessage, 0, reader.messageCount - 1);
-            if (currentMessage != previousMessage) {
-                reader.readField(currentMessage, reader.currentField);
-                previousMessage = currentMessage;
-                updateImg = true;
-            }
-
-            ImGui::Separator();
-
-            // Field information
-            ImGui::Text("Field: %s (%s) (indicatorOfParameter: %ld) on typeOfLevel %s", reader.currentField.name.c_str(), reader.currentField.shortName.c_str(),
-                        reader.currentField.indicatorOfParameter, reader.currentField.indicatorOfTypeOfLevel.c_str());
-            ImGui::Text("    parameterNumber = %ld, category = %ld, discipline = %ld", reader.currentField.parameterNumber, 
-                reader.currentField.parameterCategory, reader.currentField.discipline);
-            ImGui::Text("Level: %ld", reader.currentField.level);
-            ImGui::Text("    Type of level: %s (typeOfFirstFixedSurface: %s)", reader.currentField.typeOfLevel.c_str(), reader.currentField.typeOfFirstFixedSurface.c_str());
-            ImGui::Text("Units: %s", reader.currentField.units.c_str());
-            ImGui::Text("Dimensions: %ld x %ld", reader.currentField.width, reader.currentField.height);
-            ImGui::Text("Value range: %.6f to %.6f", reader.currentField.min_value, reader.currentField.max_value);
-
-            ImGui::Separator();
-
-            // Visualization
-            ImGui::Begin("Visualization Settings");
-            {
-                ImGui::Text("Visualization:");
-                ImGui::SliderInt("Display Zoom Factor", &settings.displayZoomFactor, 1, 10);
-                ImGui::Checkbox("Use custom min and max", &settings.useCustomMinMax);
-                ImGui::InputFloat("Minimum", &settings.minVal, 0.f, 0.f, "%.8f");
-                ImGui::InputFloat("Maximum", &settings.maxVal, 0.f, 0.f, "%.8f");
-                ImGui::Checkbox("Use sqrt scaling", &settings.sqrtScale);
-                ImGui::Checkbox("Use discrete colors", &settings.discreteColors);
-                ImGui::InputInt("Color count (for discrete)", (int*)&settings.colorCount);
-                ImGui::Checkbox("Old color bug (for aesthetics)", &settings.oldColorBug);
-            }
-            ImGui::End();
-            static size_t currentGradient = 0;
-            static size_t previousGradient = 0;
-            if (ImGui::BeginCombo("Gradient", mpl_gradient_names[currentGradient].c_str()))
-            {
-                for (size_t i = 0; i < mpl_gradient_names.size(); ++i)
-                {
-                    bool is_selected = (currentGradient == i);
-
-                    if (ImGui::Selectable(mpl_gradient_names[i].c_str(), is_selected))
-                    {
-                        currentGradient = i;
-                        settings.gradient = mpl_gradients[i];
-                    }
-
-                    if (is_selected)
-                        ImGui::SetItemDefaultFocus();
-                }
-
-                ImGui::EndCombo();
-                if (currentGradient != previousGradient) {
-                    needNewTexture = true;
-                    updateCbarTexture = true;
-                    previousGradient = currentGradient;
-                }
-            }
-
-
-            ImGui::BeginChild("Visualization", ImVec2(0, 0), true);
-            
-            if (settings != settings_old) {
-                needNewTexture = true;
-                if (cmapUpgradeNeeded(settings_old, settings)) {
-                   updateCbarTexture = true;
-                }
-                settings_old = settings;
-            }
-            
-            if (needNewTexture) {
-                displayWidth = reader.currentField.width;
-                displayHeight = reader.currentField.height; 
-                imgData.resize(displayHeight * displayWidth);
-                glDeleteTextures(1, &fieldTexture);
-                fieldTexture = 0;
-                fieldTexture = renderer.createTexture(displayWidth, displayHeight);
-                needNewTexture = false;
-                updateImg = true;
-            }
-
-            if (updateImg) {
-                renderer.renderField(reader.currentField, displayWidth, displayHeight, settings, imgData);
-                renderer.updateTexture(fieldTexture, displayWidth, displayHeight, imgData);
-                updateImg = false;
-            }
-
-            if (updateCbarTexture) {
-                renderer.updateCbar(cbarTexture, cbarWidth, cbarHeight, cbarData, settings);
-                renderer.updateTexture(cbarTexture, cbarWidth, cbarHeight, cbarData);
-                updateCbarTexture = false;
-            }
-            ImGui::Image((ImTextureID)(intptr_t)cbarTexture, ImVec2(cbarWidth, cbarHeight));
-            ImGui::Image((ImTextureID)(intptr_t)fieldTexture, 
-                ImVec2(displayWidth * settings.displayZoomFactor, displayHeight * settings.displayZoomFactor), 
-                yScanDirectionA, yScanDirectionB);
-
-            
-            ImGui::EndChild();
-        } else {
-            ImGui::Text("No GRIB file loaded.");
-            ImGui::Text("Please specify a GRIB file path and click 'Load'");
-            ImGui::Text("Or run with: ./GribViewer <path-to-grib-file>");
-        }
-
-        ImGui::End();
-
-        // Rendering
-        ImGui::Render();
-        int display_w, display_h;
-        glfwGetFramebufferSize(window, &display_w, &display_h);
-        glViewport(0, 0, display_w, display_h);
-        glClearColor(0.45f, 0.55f, 0.60f, 1.00f);
-        glClear(GL_COLOR_BUFFER_BIT);
-        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-        if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
-        {
-            GLFWwindow* backup_current_context = glfwGetCurrentContext();
-            ImGui::UpdatePlatformWindows();
-            ImGui::RenderPlatformWindowsDefault();
-            glfwMakeContextCurrent(backup_current_context);
-        }
-        glfwSwapBuffers(window);
+        showMainwindow(renderer, filename, reader, yScanDirectionA, yScanDirectionB, window, io);
     }
 
     // Cleanup
@@ -319,3 +111,4 @@ int main(int argc, char** argv) {
 
     return 0;
 }
+
---
+++ src/ui/mainWindow.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <imgui.h>
+#include <GLFW/glfw3.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
+
+#include "grib_reader.h"
+#include "mpl_gradients.h"
+#include "renderer.h"
+#include "settings.h"
+
+#include "messagesWindow.h"
+
+
+void showMainwindow(Renderer& renderer, char filename[512], GribReader& reader,
+                    ImVec2& yScanDirectionA, ImVec2& yScanDirectionB, GLFWwindow* window,
+                    ImGuiIO& io) ;
+
