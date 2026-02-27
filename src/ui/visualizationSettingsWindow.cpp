f567ceac687f8b04fe3a1a6effaeb53182a2173f
phillip <ph.scheffknecht@gmail.com>
2026-02-27 08:57:18 +0000

    make custom min max and symmetric around zero mutually exclusive

--- src/ui/visualizationSettingsWindow.cpp
+++ src/ui/visualizationSettingsWindow.cpp
@@ -8,6 +8,7 @@ void visualizationSettingsWindow(GribViewerSettings& settings, GribField& field)
         ImGui::SliderInt("Display Zoom Factor", &settings.displayZoomFactor, 1, 10);
         ImGui::Checkbox("Use custom min and max", &settings.useCustomMinMax);
         if (settings.useCustomMinMax) {
+            settings.symmetricAroundZero = false;
             ImGui::InputFloat("Minimum", &settings.minVal, 0.f, 0.f, "%.8f");
             ImGui::InputFloat("Maximum", &settings.maxVal, 0.f, 0.f, "%.8f");
         }
@@ -17,6 +18,7 @@ void visualizationSettingsWindow(GribViewerSettings& settings, GribField& field)
         ImGui::InputInt("Color count (for discrete)", (int*)&settings.colorCount);
         ImGui::Checkbox("Old color bug (for aesthetics)", &settings.oldColorBug);
         if (settings.symmetricAroundZero) {
+            settings.useCustomMinMax = false;
             float absMax = std::max(std::abs(field.min_value), std::abs(field.max_value));
             settings.minVal = -absMax;
             settings.maxVal = absMax;
